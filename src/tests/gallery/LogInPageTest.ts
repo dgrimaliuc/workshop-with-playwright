c8a9283be0a3233b41ec941737a489acbff19714
denis.grimaliuc <denis.grimaliuc@crunchyroll.com>
2025-02-12 11:50:36 +0200

    Removed unused code

--- src/tests/gallery/LogInPageTest.ts
+++ src/tests/gallery/LogInPageTest.ts
@@ -1,13 +1,6 @@
 import { test, describe } from "../../internal";
-import { StreamHomePage } from "../../ui/pages/neonStream";
 
 describe("LogIn page tests", () => {
-  let logInPage: StreamHomePage = null;
-
-  test.beforeEach(async ({ app }) => {
-    logInPage = app.stream.home;
-  });
-
   test("Log in test", async ({ page, actions }) => {
     await page.getByTestId("email-input").fill("test@gmail.com");
     await page.getByTestId("password-input").fill("aaaaaa");
